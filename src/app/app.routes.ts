1c792989238d5858d32dcd0c28187b8e729ab30e
Lucio Aimar <lucioaimar@proton.me>
2024-02-06 21:02:55 -0300

    Building Monorepo

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./shared/tabs/tabs.routes').then((m) => m.routes),
-  },
-];
--- src/app/shared/pipes/playing-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlayingTimePipe } from './playing-time.pipe';
-
-describe('PlayingTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlayingTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
